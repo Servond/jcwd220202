976cf3312563d24b9fe2fdcc010f8e0e0c93bcad
dwitap <dwitap.1223@gmail.com>
2022-11-21 14:25:25 +0700

    homepage

--- projects/client/src/App.jsx
+++ projects/client/src/App.jsx
@@ -1,11 +1,53 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
+import AdminDashboard from "./pages/AdminDashboard"
+import Home from "./pages/Home"
+import CategoryList from "./pages/Category"
 
 const App = () => {
-  return (
-    <Box>
-      <Text>Hello World!</Text>
-    </Box>
-  );
-};
+    return (
+        // 430 932
+        <Box maxW={"430px"} maxH={"932px"} w={"100%"} h={"100%"}>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/admin/dashboard" element={<AdminDashboard />} />
+                <Route path="/category" element={<CategoryList />} />
+            </Routes>
+            {/* <Box
+                overflow={"hidden"}
+                position="sticky"
+                mb={"0"}
+                w={"100%"}
+                display="flex"
+                alignItems="center"
+                justifyContent={"center"}
+            >
+                <Grid
+                    w={"100%"}
+                    h={"50px"}
+                    maxW={"430px"}
+                    maxH={"932px"}
+                    templateColumns="repeat(4, 1fr)"
+                    gap={6}
+                    textAlign={"center"}
+                    bgColor={"#E07A5F"}
+                >
+                    <GridItem w="100%">
+                        <Image src={home} />
+                    </GridItem>
+                    <GridItem w="100%">
+                        <Image src={cart} />
+                    </GridItem>
+                    <GridItem w="100%">
+                        <Image src={list} />
+                    </GridItem>
+                    <GridItem w="100%">
+                        <Image src={profile} />
+                    </GridItem>
+                </Grid>
+            </Box> */}
+        </Box>
+    )
+}
 
-export default App;
+export default App
