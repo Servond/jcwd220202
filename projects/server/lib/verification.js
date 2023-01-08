8ca45175def5ef1997ff757b20a0d17602f02c82
Wibi Hanif Wibowo <wibihanifwibowo@gmail.com>
2023-01-08 23:12:01 +0700

    finishing wibi 1-8-2023

--- projects/server/lib/verification.js
+++ projects/server/lib/verification.js
@@ -1,10 +1,6 @@
 const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
 
-dotenv.config();
-
-// const VERIFICATION_KEY = "123123";
-const VERIFICATION_KEY = `${process.env.SECRET_KEY}`;
+const VERIFICATION_KEY = "123123";
 
 const createVerificationToken = (payload) => {
   return jwt.sign(payload, VERIFICATION_KEY, { expiresIn: "15m" });
--- projects/server/src/routes/registerRoute.js
+++ projects/server/src/routes/registerRoute.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const registerContoller = require("../controllers/registerController");
 const { validateUser } = require("../middlewares/validationMiddleware");
-const { verifyToken } = require("../middlewares/loginMiddleware");
 
 const router = express.Router();
 
 router.post("/user", validateUser, registerContoller.registerUser);
 router.get("/user/verification", registerContoller.verifyUser);
-router.get("/user/reverification", verifyToken, registerContoller.reverifyUser);
-router.get(
-  "/user/reverification-account/",
-  registerContoller.verifyUserWhenLoggedIn
-);
+router.get("/user/reverification", registerContoller.reverifyUser);
 
 module.exports = router;
