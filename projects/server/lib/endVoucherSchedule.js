3d3e975fb497d1bcb66e408a873bb05a79efc204
RusaRobot <agassuper@gmail.com>
2023-01-12 21:08:51 +0700

    bug Agas

--- projects/server/lib/endVoucherSchedule.js
+++ projects/server/lib/endVoucherSchedule.js
@@ -4,33 +4,33 @@ const moment = require("moment");
 const { Op } = require("sequelize");
 
 const endVoucherScheduler = schedule.scheduleJob("0 * * * * *", async () => {
-  const getVoucher = await db.Voucher.findAll({
-    where: {
-      [Op.or]: [
-        {
-          voucher_end_date: moment(),
-        },
-        {
-          voucher_end_date: {
-            [Op.lt]: moment(),
-          },
-        },
-      ],
-    },
-  });
+  // const getVoucher = await db.Voucher.findAll({
+  //   where: {
+  //     [Op.or]: [
+  //       {
+  //         voucher_end_date: moment(),
+  //       },
+  //       {
+  //         voucher_end_date: {
+  //           [Op.lt]: moment(),
+  //         },
+  //       },
+  //     ],
+  //   },
+  // });
 
-  const ids = getVoucher.map((val) => val.id);
+  // const ids = getVoucher.map((val) => val.id);
 
-  await db.Voucher.update(
-    {
-      is_Active: 0,
-    },
-    {
-      where: {
-        id: ids,
-      },
-    }
-  );
+  // await db.Voucher.update(
+  //   {
+  //     is_Active: 0,
+  //   },
+  //   {
+  //     where: {
+  //       id: ids,
+  //     },
+  //   }
+  // );
 });
 
 module.exports = endVoucherScheduler;
